9550878ef88b0085bae09ce372e23aa1c80e0c87
Ibrahim Sadik Tamim <66836027+tam11a@users.noreply.github.com>
2021-04-21 02:08:02 +0530

    #3

--- statics/js/services_hover.js
+++ statics/js/services_hover.js
@@ -1,7 +1,9 @@
 $(".row-cube").mouseenter(function(){
     document.querySelector("#watermark-here").innerHTML = $(this).html();
+    document.querySelector(".services-icon").classList.remove("show-svg");
   });
 
 $(".row-cube").mouseleave(function(){
     document.querySelector("#watermark-here").innerHTML = "";
+    document.querySelector(".services-icon").classList.add("show-svg");
   });
